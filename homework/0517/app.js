fce98a84cdfe0487ae8687a78b1cc06e65f5470e
jbyun <binifia@gmail.com>
2022-05-22 11:31:07 +0900

    0522 : files modified

--- homework/0517/app.js
+++ homework/0517/app.js
@@ -1,10 +1,11 @@
+// Event Listener 를 통해 로또 번호 가져오는 이벤트 수행.
 document.addEventListener("DOMContentLoaded", () => {
-  let doc = document.getElementById("start");
+  let doc = document.querySelector("#start");
 
+  // 최종 당첨번호 6개를 담을 배열
   let lotto = [];
-  let i;
 
-  for (i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     let num = parseInt(Math.floor(Math.random() * 45)) + 1;
     if (lotto.indexOf(num) < 0) {
       lotto[i] = num + " ";
--- homework/0518/js/app.js
+++ homework/0518/js/app.js
@@ -5,21 +5,8 @@ function deckOfCards() {
   // 모양 4가지를 담은 배열1
   const shape = ["S", "H", "D", "C"];
   // 숫자 13가지를 담은 배열2
-  const numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "T",
-    "J",
-    "Q",
-    "K",
-  ];
+  const numbers = ["A", "2", "3", "4", "5", "6", 
+                   "7", "8", "9", "T", "J", "Q", "K" ];
 
   //   최종 카드 set을 담을 배열
   const cardCase = [];
